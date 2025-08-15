4e129ecee98c66e3843b0f1ec26b1f42c32e0b53
yogaprasadk <yogaprasadf12@gmail.com>
2025-08-15 22:53:35 +0530

    hashing started

---
+++ Hashing/Introduction/Hashmap.java
@@ -0,0 +1,16 @@
+
+import java.util.HashMap;
+
+public class Hashmap{
+    public static void main(String[] args) {
+        HashMap<String,Integer> ans =  new HashMap<>();
+        // create 
+        ans.put("Lee", 12);
+        ans.put("ranjan", 1);
+        ans.put("Rio", 13);
+        System.out.println(ans);
+
+        // get
+        // System.out.println(ans.get(1));
+    }
+}
