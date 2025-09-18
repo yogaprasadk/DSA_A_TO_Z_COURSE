c45e6acbcc67c4be8fc481b21970c46003c8e2b5
yogaprasadk <yogaprasadf12@gmail.com>
2025-09-18 22:12:37 +0530

    array problems remaining

---
+++ Arrays/Problems/ProductOfArrayExceptSelf.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+public class ProductOfArrayExceptSelf {
+    // https://leetcode.com/problems/product-of-array-except-self/
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int[] ans =  productExceptSelf(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    public static int[] productExceptSelf(int[] nums)
+    {
+        // Tc - O(N) SC - O(N)
+        int n = nums.length;
+        // Array to store all left multiplication
+        int[] left = new int[n];
+        // Array to store all right multiplication
+        int[] right = new int[n];
+
+        left[0] = 1;
+        for (int i = 1;i<n;i++){
+            left[i] = left[i-1] * nums[i-1];
+        }
+
+        right[n-1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i+1] * nums[i+1];         
+        }
+
+        int[] ans = new int[n];
+        for (int index = 0; index < n; index++) {
+            ans[index] = left[index] * right[index];
+        }
+        return ans;
+    }
+
+    //brute
+    /*
+     
+        public static int[] productOfArrayExceptItself(int[] arr){
+                TC - O(n2) Sc - O(n)
+                int[] ans = new int[arrlength];
+                for(int i = 0;i<arr.length;i++){
+                    ans[i] = 1;
+                }
+                for(int i = 0;i<arr.length;i++){
+                    for(int j = 0;j<arr.length;j++){
+                        ans[i] = ans[i] * arr[j];                     
+                    }
+                }
+                return ans;
+        }
+
+
+
+
+
+
+
+
+
+
+     */
+}
